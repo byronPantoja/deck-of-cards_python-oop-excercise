068c5726f5efe89378676f9da5aa65c7b029ecd0
“dbp” <byronpantoja@gmail.com>
2021-01-08 07:49:43 +0800

    Update card_deck.py
    Adding Deck Class and testing

--- card_deck.py
+++ card_deck.py
@@ -8,3 +8,15 @@ class Card:
 
 
 print(Card("A", "Clubs"))
+
+
+class Deck:
+    def __init__(self):
+        suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
+        values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+        self.cards = [Card(value, suit) for suit in suits for value in values]
+
+        print(self.cards)
+
+
+Deck()
