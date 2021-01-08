5735cea8b9aececbb3b8eda02875e47de13d1597
“dbp” <byronpantoja@gmail.com>
2021-01-08 08:33:07 +0800

    Update card_deck.py
    Adding _deal and testing logic

--- card_deck.py
+++ card_deck.py
@@ -15,12 +15,25 @@ class Deck:
         suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
         values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
         self.cards = [Card(value, suit) for suit in suits for value in values]
-
         print(self.cards)
+
+    def __repr__(self):
+        return f"This is a {d.count()} card deck."
 
     def count(self):
         return len(self.cards)
 
+    def _deal(self, num):
+        count = self.count()
+        actual = min([count, num])
+        if count == 0:
+            return ValueError("All cards have been dealt!")
+        cards = self.cards[-actual:]
+        self.cards = self.cards[:-actual]
+        return cards
+
 
 d = Deck()
-print(f"This is a {d.count()} card deck.")
+print(d._deal(52))
+print(d.count())
+print(d._deal(52))
